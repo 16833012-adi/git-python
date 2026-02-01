18d81f053d3d5d248f2dcf9118eb5e1212c6c8b1
16833012-adi <16833012@aru.ac.th>
2026-02-01 14:37:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,10 @@ y = 20
 add = module.add(x,y)
 subtract = module.subtract(y,x)
 multiply = module.multiply(y,x)
-
-print(f"The add is: {add}")
-print(f"The sub is: {subtract}")
-print(f"The sub is: {multiply}")
 divide = module.divide(y,x)
 
 
 print(f"The add is: {add}")
 print(f"The sub is: {subtract}")
+print(f"The sub is: {multiply}")
 print(f"The divide is: {divide}")
