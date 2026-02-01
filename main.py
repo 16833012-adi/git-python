9e9b2088498ae2f7ffa7ee2d230a8bb22ea4a9b6
16833012-adi <16833012@aru.ac.th>
2026-02-01 14:00:54 +0700

    add

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ x = 10
 y = 20
 
 add = module.add(x,y)
-subtract = module(y,x)
-print("The add is: {add}")
-print("The sub is: {subtract}")
+subtract = module.subtract(y,x)
+
+print(f"The add is: {add}")
+print(f"The sub is: {subtract}")
